8b9e739c3c0049df7020d486567b623817b1bad0
davill02 <49804801+davill02@users.noreply.github.com>
2021-02-20 22:49:47 +0300

    Add ellipses stuff functions

--- Task1/src/main/java/ellipses/Circle.java
+++ Task1/src/main/java/ellipses/Circle.java
@@ -3,9 +3,29 @@ package ellipses;
 import java.awt.*;
 
 public class Circle extends Ellipse {
-
+    //Point B is unused
+    //Location is center
+    //Point A is some point
     public Circle() {
 
+    }
+
+    public Circle(Point location, Color lineColor, Color fillColor, Point pointA, Point pointB) {
+        super(location, lineColor, fillColor, pointA, pointB);
+    }
+
+    private int calculateRadius(){
+        Point p1 = getLocation();
+        Point p2 = getPointA();
+        return (int) Math.round(Math.sqrt((p1.x - p2.x)*(p1.x - p2.x) + p1.y - p2.y)*(p1.y - p2.y));
+    }
+
+    @Override
+    protected Point getUpLeftCorner(){
+        Point upLeftCorner = new Point(0,0);
+        upLeftCorner.x = getLocation().x - calculateRadius();
+        upLeftCorner.y = getLocation().y - calculateRadius();
+        return upLeftCorner;
     }
 
     public void draw(Graphics g) {
--- Task1/src/main/java/ellipses/Ellipse.java
+++ Task1/src/main/java/ellipses/Ellipse.java
@@ -6,47 +6,72 @@ import java.awt.*;
 
 public class Ellipse extends Figure2D {
 
-	private Point pointA;
-	private Point pointB;
+    private Point pointA;
+    private Point pointB;
+    //Point A and B MUST have one same coordinates with location
+    //Point A and B are points on the ellipse
+    //Point A and B must be on the different axes
 
-	public Ellipse(){
-		super();
-		this.pointA = new Point(0,0);
-		this.pointB = new Point(0,0);
-	}
 
-	public Ellipse(Point pointA, Point pointB) {
-		this.pointA = pointA;
-		this.pointB = pointB;
-	}
+    protected Point getUpLeftCorner() {
+        Point upLeftCorner = new Point(0, 0);
+        upLeftCorner.x = getLocation().x - getWidth() / 2;
+        upLeftCorner.y = getLocation().y - getHeight() / 2;
+        return upLeftCorner;
+    }
 
-	public Ellipse(Color fillColor, Point pointA, Point pointB) {
-		super(fillColor);
-		this.pointA = pointA;
-		this.pointB = pointB;
-	}
+    private int getHeight() {
+        if (2 * Math.abs(pointA.y - getLocation().y) != 0) {
+            return 2 * Math.abs(pointA.y - getLocation().y);
+        }
+        return 2 * Math.abs(pointB.y - getLocation().y);
+    }
 
-	public Ellipse(Point location, Color lineColor, Color fillColor, Point pointA, Point pointB) {
-		super(location, lineColor, fillColor);
-		this.pointA = pointA;
-		this.pointB = pointB;
-	}
+    private int getWidth() {
+        if (2 * Math.abs(pointA.x - getLocation().x) != 0) {
+            return 2 * Math.abs(pointA.x - getLocation().x);
+        }
+        return 2 * Math.abs(pointB.x - getLocation().x);
+    }
 
-	public void draw(Graphics g){
+    public Ellipse() {
+        super();
+        this.pointA = new Point(0, 0);
+        this.pointB = new Point(0, 0);
+    }
 
-	}
+    public Ellipse(Point pointA, Point pointB) {
+        this.pointA = pointA;
+        this.pointB = pointB;
+    }
 
-	@Override
-	public void move(int offsetX, int offsetY) {
+    public Ellipse(Color fillColor, Point pointA, Point pointB) {
+        super(fillColor);
+        this.pointA = pointA;
+        this.pointB = pointB;
+    }
 
-	}
+    public Ellipse(Point location, Color lineColor, Color fillColor, Point pointA, Point pointB) {
+        super(location, lineColor, fillColor);
+        this.pointA = pointA;
+        this.pointB = pointB;
+    }
 
-	public Point getPointA(){
-		return pointA;
-	}
+    public void draw(Graphics g) {
 
-	public Point getPointB(){
-		return pointB;
-	}
+    }
+
+    @Override
+    public void move(int offsetX, int offsetY) {
+
+    }
+
+    public Point getPointA() {
+        return pointA;
+    }
+
+    public Point getPointB() {
+        return pointB;
+    }
 
 }
