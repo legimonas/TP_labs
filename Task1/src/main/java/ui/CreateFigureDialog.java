c24293529a8e837cd0ce06295b9ac5e5dd30d6b8
legimonas <legimonas100@mail.ru>
2021-03-05 22:11:47 +0300

    Added createChangePanel function

--- Task1/src/main/java/ui/CreateFigureDialog.java
+++ Task1/src/main/java/ui/CreateFigureDialog.java
@@ -4,6 +4,7 @@ import figures.Figure;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Objects;
 
 public class CreateFigureDialog extends JDialog {
     JPanel editPanel;
@@ -50,7 +51,7 @@ public class CreateFigureDialog extends JDialog {
         editPanel.add(point2TextField);
         editPanel.setVisible(true);
 
-
+        createChangeEditPanelListener();
 
 
         editPanel.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
@@ -59,7 +60,7 @@ public class CreateFigureDialog extends JDialog {
 
 
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        setLocation(100, 100);
+        setLocation(150, 150);
         getRootPane().setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
         pack();
         setVisible(true);
@@ -68,6 +69,55 @@ public class CreateFigureDialog extends JDialog {
     private void createChangeEditPanelListener(){
         figuresComboBox.addActionListener(e->{
             //TODO: modify edit panel for each figure
+            String selectedItem = (String) figuresComboBox.getSelectedItem();
+            switch (Objects.requireNonNull(selectedItem)){
+                case "Line":
+                    editPanel.removeAll();
+                    editPanel.setLayout(new GridLayout(2, 2));
+                    editPanel.add(new JLabel("Point 1: "));
+                    editPanel.add(point1TextField);
+                    editPanel.add(new JLabel("Point 2: "));
+                    editPanel.add(point2TextField);
+                    pack();
+                    editPanel.revalidate();
+                    break;
+                case "Ray":
+                    //TODO
+                    break;
+                case "Section":
+                    //TODO
+                    break;
+                case "Circle":
+                    //TODO
+                    break;
+                case "Ellipse":
+                    //TODO
+                    break;
+                case "Equilateral Triangle":
+                    //TODO
+                    break;
+                case "Polygon":
+                    editPanel.removeAll();
+                    editPanel.setLayout(new GridLayout(1, 2));
+                    editPanel.add(new JLabel("Enter points, separated by space or \",\": "));
+                    editPanel.add(pointsTextField);
+                    pack();
+                    editPanel.revalidate();
+                    break;
+                case "Rectangle":
+                    //TODO
+                    break;
+                case "Regular Polygon":
+                    //TODO
+                    break;
+                case "Regular Triangle":
+                    //TODO
+                    break;
+                case "Rhombus":
+                    //TODO
+                    break;
+
+            }
         });
     }
 
