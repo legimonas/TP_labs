ecbf2b5473cc3ab7630a9257bea389d7f05c215d
davill02 <49804801+davill02@users.noreply.github.com>
2021-05-23 21:49:29 +0300

    Change server part

--- task2/src/main/resources/data.sql
+++ task2/src/main/resources/data.sql
@@ -1,4 +1,4 @@
 insert into user(id, login, password, role)
-values (1, 'davill02', '1359', 'Albanian coder'),
-       (2, 'legimonas100', '1234', '80000lvl scribbler'),
-       (3, 'cralia', '8888', 'Main supervisor');
+values (1, 'davill02', '1359', 'PRODUCT_MANAGER'),
+       (2, 'legimonas100', '1234', 'CATALOG_MANAGER'),
+       (3, 'cralia', '8888', 'TRANSPORT_MANAGER');
